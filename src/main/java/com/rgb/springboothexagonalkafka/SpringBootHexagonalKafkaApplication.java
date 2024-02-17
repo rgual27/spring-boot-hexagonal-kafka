37b1a04da91e0ab8b632335e43249c927c35de1f
rgual27 <rgual27@gmail.com>
2024-02-17 14:18:05 +0100

    create archetype

---
+++ src/main/java/com/rgb/springboothexagonalkafka/SpringBootHexagonalKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.rgb.springboothexagonalkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHexagonalKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHexagonalKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rgb/springboothexagonalkafka/SpringBootHexagonalKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rgb.springboothexagonalkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHexagonalKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
